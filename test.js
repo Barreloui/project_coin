5ff4db52fdf4ece2298dc0a71c9b367c8e073756
Barreloui <58188242+Barreloui@users.noreply.github.com>
2020-06-01 10:45:12 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,14 @@
+
+describe('', function() {
+    describe('', function() {
+
+        it('', ()=> {
+            const newProduct = new ProductService().add(...);
+            expect(newProduct.status).to.equal('pendingApproval');
+        });
+    });
+});
+    const color = require("./color");
+    test("color", () = {
+        expect(color).toBe("black");
+    }};
